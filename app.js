d69cd4b617b6d7bfd8f417ae0968c6d74c7e32d8
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-03-17 16:02:34 +0000

    Update selector

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // jQuery methods go here...
   //	console.log('Hello World!');
-  	$('click-me').click(function () {
+  	$('#click-me').click(function () {
   		console.log('button clicked');
   	});
 });
