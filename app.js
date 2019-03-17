724f20b8718a5bc24ed9acc066787ccb5967a122
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-03-17 16:17:39 +0000

    First ajax call

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ $(document).ready(function(){
   //	console.log('Hello World!');
   	$('#click-me').click(function () {
   		console.log('button clicked');
+  		$.get( "building.json", function( data ) {
+  			$( "#result" ).html( data );
+  			alert( "Load was performed." );
+		});
   	});
 });
