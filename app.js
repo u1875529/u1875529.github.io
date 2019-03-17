7331f3925a5ab94ad882e335b086baed341a4d52
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-03-17 16:28:41 +0000

    Update jquery

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ $(document).ready(function(){
   // jQuery methods go here...
   //	console.log('Hello World!');
   	$('#click-me').click(function () {
-  		console.log('button clicked');
   		$.get( 'building.json', function( data ) {
-  			$( '#result' ).html( data );
+  			$( '#result' ).text( data );
   			console.log('DATA: ', data);
-  			alert( "Load was performed." );
-
 		});
   	});
 });
