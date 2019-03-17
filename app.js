acf1267381e0a8678904c22774d05d2691e5c919
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-03-17 16:00:31 +0000

    Button added

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
   // jQuery methods go here...
-  	console.log('Hello World!');
+  //	console.log('Hello World!');
+  	$('click-me').click(function () {
+  		console.log('button clicked');
+  	});
 });
