afce5fa143ce298eee2912585ce95dfb7765c1e2
u18775529 <43642672+u18775529@users.noreply.github.com>
2019-03-25 16:53:45 +0000

    Commit - installed Less
    Installed language extension Less for use with CSS

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var buildingData;
-
-$(document).ready(function(){
-
-  // jQuery methods go here...
-  //	console.log('Hello World!');
-  	$('#loadData').click(function () {
-  		$.get( 'building.json', function( data ) {
-  			buildingData = data;
-  			updateElement(buildingData[0])
-		});
-  	});
-
-  	$('#nextBuilding').click(function () {
-  		updateElement(buildingData[1])
+	$(document).ready(function(){
+	// jQuery methods go here...
+	// console.log('Hello World!');
+		$('#loadData').click(loadData);
+			$('#nextBuilding').click(function () {
+				updateElement(buildingData[1])
   	});
 });
 
 function updateElement(data) {
 	$( '#result' ).text( JSON.stringify(data.description) );
   	$( '#buildingImage').attr('src', data.imageUrl);
+
+
+function loadData () {
+	$.get( 'building.json', function( data ) {
+  			buildingData = data;
+  			updateElement(buildingData[0])
+		});
 }
